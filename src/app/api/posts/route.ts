3523b1f61d2f54f024fef2afeebe7c608ac51a27
jetpogi21 <jet_pradas@yahoo.com>
2023-06-07 10:19:15 +0800

    new file:   middleware.ts 	modified:   src/app/api/posts/route.ts

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -4,12 +4,12 @@ import Post from "@/models/Post";
 
 export const GET = async (req: NextRequest) => {
   try {
-    await connect();
+    /* await connect();
 
-    const posts = await Post.find();
-    return new NextResponse(posts, { status: 200 });
+    const posts = await Post.find(); */
+    return NextResponse.json({ data: "Hello" }, { status: 200 });
   } catch (err) {
     /* console.log(err); */
-    return new NextResponse("Database Error", { status: 500 });
+    return NextResponse.json({ data: "Database Error" }, { status: 500 });
   }
 };
